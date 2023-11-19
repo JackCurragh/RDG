544c96c14671712db0478ad206e73cf1f11c8d95
Jack Tierney <jack.tierney@student.ucc.ie>
2023-11-19 17:18:18 +0000

    added test for from fasta

--- tests/test_build_from_sequence.py
+++ tests/test_build_from_sequence.py
@@ -32,23 +32,7 @@ def test_parse_sequence_into_translated_regions():
     result = parse_sequence_into_translated_regions(sequence, min_length=100)
     assert result == []
 
-# def test_build_graphs_from_fasta(example_fasta_path):
-#     # Test case 1: Minimal input
-#     result = build_graphs_from_fasta(example_fasta_path)
-#     assert result == [(0, 3)]
-
-#     # Test case 2: No start codon
-#     result = build_graphs_from_fasta(example_fasta_path)
-#     assert result == []
-
-#     # Test case 3: Multiple start and stop codons
-#     result = build_graphs_from_fasta(example_fasta_path)
-#     assert result == [(0, 21), (3, 12)]
-
-#     # Test case 4: Custom start codons
-#     result = build_graphs_from_fasta(example_fasta_path, starts=["CTG"])
-#     assert result == [(3, 15)]
-
-#     # Test case 5: Custom minimum length
-#     result = build_graphs_from_fasta(example_fasta_path, min_length=15)
-#     assert result == [(0, 21)]
+def test_build_graphs_from_fasta(example_fasta_path):
+    # Test case 1: Minimal input
+    result = build_graphs_from_fasta(example_fasta_path, start_codons=["ATG"], min_lenth=100)
+    assert result[0].newick() == '(((5:175)4:378,((8:63)7:333,((11:175)10:189,((14:63)13:201,2:264)12:100)9:32)6:157)3:337)1;'
