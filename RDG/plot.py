68341b8ed65d1567c330a961eb529a04c7847b90
Jack Tierney <jack.tierney@student.ucc.ie>
2024-01-30 17:58:23 +0000

    cladogram plotting

--- RDG/plot.py
+++ RDG/plot.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-import networkx as nx
 from RDG import RDG
 
 # from RDG_to_file import save, load, newick_to_file
@@ -11,80 +10,125 @@ from matplotlib.gridspec import GridSpec
 def cladogram_layout(graph: RDG) -> dict:
     """
     Method for the cladogram inspired RDG layout
-    
+
     :param graph: The RDG object representing the decision graph
     :type graph: RDG
-    
+
     :return: A dictionary representing the layout of the cladogram
-    :rtype: dict 
+    :rtype: dict
     """
-    pos = {} # node: (x, y)
+    pos = {}  # node: (x, y)
 
     # endpoints define the height of the graph
-    # it is unclear how to order the nodes to ensure no crossing edges
+    # The order of the endpoints ius determined by the upstream branch points
+    # of the endpoints
+    # The first endpoint is the one with the furthest 3' upstream branch point
     endpoints = graph.get_endpoints()
-    for i, endpoint in enumerate(endpoints):
-        pos[endpoint] = (graph.nodes[endpoint].node_start, i)
-    
+    upstream_branches = {}
+    branch_positions = {}
+    for endpoint in endpoints:
+        branch = graph.get_upstream_branchpoint(endpoint)
+        branch_positions[branch] = graph.nodes[branch].node_start
+        if branch not in upstream_branches:
+            upstream_branches[branch] = [endpoint]
+        else:
+            upstream_branches[branch].append(endpoint)
+
+    for branch in sorted(branch_positions, key=branch_positions.get,
+                         reverse=True):
+        for endpoint in upstream_branches[branch]:
+            pos[endpoint] = (graph.nodes[endpoint].node_start, len(pos))
+
+    while len(upstream_branches) >= 1:
+        for branchpoint in upstream_branches:
+            if len(upstream_branches[branchpoint]) == 2:
+                # if the branchpoint has 2 downstream nodes then it is a
+                # branchpoint and the nodes are placed at the same height
+                pos[branchpoint] = (
+                    graph.nodes[branchpoint].node_start,
+                    (
+                        pos[upstream_branches[branchpoint][0]][1]
+                        + pos[upstream_branches[branchpoint][1]][1]
+                        ) / 2,
+                )
+                upstream_branch = graph.get_upstream_branchpoint(branchpoint)
+                if upstream_branch not in upstream_branches:
+                    upstream_branches[upstream_branch] = [branchpoint]
+                else:
+                    upstream_branches[upstream_branch].append(branchpoint)
+                del upstream_branches[branchpoint]
+                break
+
+        if branchpoint in graph.get_startpoints():
+            break
+
     startpoints = graph.get_startpoints()
-    startpoint_scale = len(endpoints) / (len(startpoints) + 1) 
-    for i, startpoint in enumerate(startpoints):
-        pos[startpoint] = (graph.nodes[startpoint].node_start, i + 1 * startpoint_scale)
+    for startpoint in startpoints:
+        out_node = graph.nodes[startpoint].output_nodes[0]
+        pos[startpoint] = (
+            graph.nodes[startpoint].node_start, pos[out_node][1]
+            )
 
     unassigned_nodes = [node for node in graph.nodes if node not in pos]
-
-    for node in endpoints:
-        upstream_nodes = graph.nodes[node].input_nodes
-        for upstream_node in upstream_nodes:
-            if upstream_node not in pos:
-                if graph.nodes[upstream_node].node_type == "stop":
-                    pos[upstream_node] = (graph.nodes[upstream_node].node_start, pos[node][1])
-                    start_node = graph.nodes[upstream_node].input_nodes[0]
-                    pos[start_node] = (graph.nodes[start_node].node_start, pos[node][1]) 
-    
-    for node in graph.nodes:
-        if node not in pos:
-            pos[node] = (graph.nodes[node].node_start, len(endpoints) / 2)
-    return pos
-
-def layout_graph(graph: RDG, branch_height=1) -> dict:
-    """
-    Method for laying out the graph based on branch points
-
-    """
-    pos = {}
-
-    paths = graph.get_unique_paths()
-    node_y_positions = {paths[0][0]: 0}
-
-    branch_points = graph.get_branch_points()
-
-    branch_num = 0
-    for path in paths:
-        for i, node in enumerate(path):
-            if node in branch_points:
-                if node not in node_y_positions:
-                    node_y_positions[node] = branch_num
-                    branch_num += branch_height
-
-            else:
-                if node not in node_y_positions:
-                    if graph.nodes[node].node_type == "3_prime":
-                        upstream_node = graph.nodes[node].input_nodes[0]
-                        if upstream_node in branch_points:
-                            node_y_positions[node] = node_y_positions[path[i - 1]] + 3
-                        else:
-                            node_y_positions[node] = node_y_positions[path[i - 1]]
-                    else:
-                        node_y_positions[node] = node_y_positions[path[i - 1]]
-    for node in graph.nodes:
-        pos[node] = (graph.nodes[node].node_start, node_y_positions[node])
+    for node in unassigned_nodes:
+        upstream = graph.nodes[node].input_nodes[0]
+        pos[node] = (graph.nodes[node].node_start, pos[upstream][1])
 
     return pos
+
+
+# used in the calculation of branch heights this function looks downstream of
+# a node and returns the first end or branch node it finds.
+# if the node itself is a branch or end node it returns itself as this is run
+# on the downstream nodes of a branch point already
+def get_end_or_branch(graph, node):
+    '''
+    Get the end or branch node immediately downstream of a node
+
+    :param graph: The RDG object representing the decision graph
+    :type graph: RDG
+    :param node: The node to get the end or branch node of
+    :type node: int
+
+    :return: The end or branch node of the node
+    :rtype: int
+    '''
+    if node in graph.get_endpoints() or node in graph.get_branch_points():
+        return node
+    for downstream_node in graph.nodes[node].output_nodes:
+        if downstream_node in graph.get_endpoints() \
+          or downstream_node in graph.get_branch_points():
+            return downstream_node
+        else:
+            return get_end_or_branch(graph, downstream_node)
+
+
+# Once node positions are know this function can be used to
+# calculate the heights of the vertical branches that can be
+# used to connect translons and non coding edges
+def get_branch_heights(graph, pos):
+    '''
+    calculate the heights of the vertical branches that connect
+    translons and non coding edges at branch points
+
+    :param graph: The RDG object representing the decision graph
+    :type graph: RDG
+
+    :return: A dictionary of branch heights
+    :rtype: dict
+    '''
+    branch_heights = {}
+    for branch in graph.get_branch_points():
+        A = get_end_or_branch(graph, graph.nodes[branch].output_nodes[0])
+        B = get_end_or_branch(graph, graph.nodes[branch].output_nodes[1])
+        branch_heights[branch] = abs(pos[A][1] - pos[B][1])
+    return branch_heights
 
 
 default_color_dict = {
-    "edge_colors": {"frame0": (1, 0, 0), "frame1": (0, 1, 0), "frame2": (0, 0, 1)},
+    "edge_colors": {
+        "frame0": (1, 0, 0), "frame1": (0, 1, 0), "frame2": (0, 0, 1)
+        },
     "node_colors": {
         "startpoint": (0, 0, 1),
         "endpoint": (0.5, 0, 0.5),
@@ -98,155 +142,155 @@ default_color_dict = {
 def plot(
     graph,
     color_dict=default_color_dict,
-    node_size=360,
-    edge_width=1.5,
     height_ratios=[2, 1],
-    label_nodes=False,
-    show_non_coding=False,
+    show_non_coding=True,
+    translon_height=0.5,
+    scantron_height=0.1,
 ):
-    # The networkx graph must either be directed or the edges must be sorted before adding to graph
-    G = nx.DiGraph()
+    '''
+    Generate a plot of the RDG
 
-    # store orfs in each frame for plotting the ORF plot.
-    orfs_in_frame = {0: [], 1: [], 2: []}
-    orfs = graph.get_orfs()
-    for orf in orfs:
-        if len(orf) == 2:
-            orfs_in_frame[orf[0] % 3].append((orf[0], orf[1] - orf[0]))
-        elif len(orf) == 3:
-            orfs_in_frame[orf[0] % 3].append((orf[0], orf[1] - orf[0]))
-            orfs_in_frame[orf[1] % 3].append((orf[1], orf[2] - orf[1]))
+    :param graph: The RDG object representing the decision graph
+    :type graph: RDG
+
+    :param color_dict: A dictionary of colors to use for the plot
+    :type color_dict: dict
+
+    :param height_ratios: The height ratios of the two subplots
+    :type height_ratios: list
+
+    :param show_non_coding: Whether to show the non coding edges
+    :type show_non_coding: bool
+
+    :param translon_height: The height of the translon plot
+    :type translon_height: float
+
+    :param scantron_height: The height of the non coding edges
+    :type scantron_height: float
+
+    :return: The figure and axes objects
+    :rtype: tuple
+    '''
+
+    # store translons in each frame for plotting the translon plot.
+    translons_in_frame = {0: [], 1: [], 2: []}
+    translons = graph.get_translons()
+    for translon in translons:
+        if len(translon) == 2:
+            translons_in_frame[translon[0] % 3].append(
+                (translon[0], translon[1] - translon[0])
+                )
+        elif len(translon) == 3:
+            translons_in_frame[translon[0] % 3].append(
+                (translon[0], translon[1] - translon[0])
+                )
+            translons_in_frame[translon[1] % 3].append(
+                (translon[1], translon[2] - translon[1])
+                )
 
     # position nodes on xy plane
     pos = cladogram_layout(graph)
-    # pos = layout_graph(graph, branch_height=2)
-
-    # identify feature types for colouring
-    endpoints = graph.get_endpoints()
-    startpoints = graph.get_startpoints()
-    translation_starts = graph.get_start_nodes()
-    translation_stops = graph.get_stop_nodes()
-    frameshifts = graph.get_frameshifts()
 
     if show_non_coding:
         pass
     else:
         graph.remove_edge(1)
 
-    edges = graph.get_edges_from_to()
-    # loop over edges and make translated edges 10x thicker than non-translated edges
-    for edge in edges:
-        if graph.edges[edges[edge]].edge_type == "translated":
-            G.add_edge(
-                graph.edges[edges[edge]].from_node,
-                graph.edges[edges[edge]].to_node,
-                weight=20,
-            )
-        else:
-            G.add_edge(
-                graph.edges[edges[edge]].from_node,
-                graph.edges[edges[edge]].to_node,
-                weight=2,
-            )
-    edge_widths = [d["weight"] for u, v, d in G.edges(data=True)]
-
-    # assign correct colouring based on frame to each ORF
-    # G.add_edges_from(edges.keys())
-
-    node_colors = []
-    for node in G.nodes():
-        if node in startpoints:
-            node_colors.append(color_dict["node_colors"]["startpoint"])
-        elif node in endpoints:
-            node_colors.append(color_dict["node_colors"]["endpoint"])
-        elif node in translation_starts:
-            node_colors.append(color_dict["node_colors"]["translation_start"])
-        elif node in translation_stops:
-            node_colors.append(color_dict["node_colors"]["translation_stop"])
-        elif node in frameshifts:
-            node_colors.append(color_dict["node_colors"]["frameshift"])
-        else:
-            node_colors.append((0, 0, 0))
-
-    edge_colors = []
-
-    for edge in G.edges:
-        if graph.edges[edges[edge]].edge_type == "translated":
-            frame = graph.edges[edges[edge]].frame
-        else:
-            frame = None
-
-        if frame == 0:
-            edge_colors.append(color_dict["edge_colors"]["frame0"])
-        elif frame == 1:
-            edge_colors.append(color_dict["edge_colors"]["frame1"])
-        elif frame == 2:
-            edge_colors.append(color_dict["edge_colors"]["frame2"])
-        else:
-            edge_colors.append((0, 0, 0))
-
     # set up the figure
     fig = plt.figure()
     fig.suptitle(f"Visualisation of an RDG for {graph.locus}")
 
-    gs = GridSpec(2, 1, width_ratios=[1], height_ratios=height_ratios, hspace=0)
+    gs = GridSpec(2, 1, width_ratios=[1],
+                  height_ratios=height_ratios, hspace=0)
     ax1 = fig.add_subplot(gs[0])
     ax2 = fig.add_subplot(gs[1])
 
-    node_sizes = {node: node_size if G.degree(node) > 1 else 0 for node in G.nodes()}
+    # Turn off axis labels
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    ax1.set_xlabel('')
+    ax1.set_ylabel('')
 
-    # plot the graph
-    nx.draw_networkx(
-        G,
-        pos=pos,
-        ax=ax1,
-        node_shape="s",
-        node_size=0,
-        node_color=node_colors,
-        width=edge_widths,
-        edge_color=edge_colors,
-        with_labels=label_nodes,
-        alpha=0.0,
-        font_size=15,
-        arrows=False,
-    )
+    max_x = max([pos[node][0] for node in pos])
+    ax1.set_xlim(0, max_x)
+    ax2.set_xlim(0, max_x)
 
-    for edge in G.edges():
-        source, target = edge
-        rad = 0.2
-        arrowprops=dict(lw=G.edges[(source,target)]['weight'],
-                        arrowstyle="-",
-                        color='blue',
-                        connectionstyle=f"Angle,angleA=0,angleB=90,rad=0.0",
-                        linestyle= '-',
-                        alpha=0.6)
-        ax1.annotate("",
-                    xy=pos[source],
-                    xytext=pos[target],
-                    arrowprops=arrowprops
+    max_y = max([pos[node][1] for node in pos])
+    ax1.set_ylim(0, max_y+2)
+    ax2.set_ylim(0, max_y)
+
+    branch_heights = get_branch_heights(graph, pos)
+    for branch in graph.get_branch_points():
+        if branch in pos:
+            A = get_end_or_branch(graph, graph.nodes[branch].output_nodes[0])
+            B = get_end_or_branch(graph, graph.nodes[branch].output_nodes[1])
+            base_height = min(pos[A][1], pos[B][1])
+            rect = patches.Rectangle(
+                (pos[branch][0], base_height),
+                width=20,
+                height=branch_heights[branch],
+                linewidth=0.5,
+                edgecolor='#000000',
+                facecolor='#000000',
                 )
-   ##  ATF4 heights
-    # for node, size in node_sizes.items():
-    #     if node in translation_starts:
-    #         rect = patches.Rectangle((pos[node][0], pos[node][1] - 0.2), 20, 1, linewidth=1, edgecolor=color_dict["node_colors"]["translation_start"], facecolor=color_dict["node_colors"]["translation_start"])
-    #         ax1.add_patch(rect)
-    #     elif node in translation_stops:
-    #         rect = patches.Rectangle((pos[node][0], pos[node][1] - 0.2), 20, 1, linewidth=1, edgecolor=color_dict["node_colors"]["translation_stop"], facecolor=color_dict["node_colors"]["translation_stop"])
-    #         ax1.add_patch(rect)
+            ax1.add_patch(rect)
 
+    # handle scaling of translon Y axis offset.
+    # If translons are plotted without offset they do not align
+    # with the non coding edges
+    translon_scaling = (translon_height / 2) - (scantron_height / 2)
 
+    edges = graph.get_edges_from_to()
+    # loop over edges and make translated edges 10x thicker than
+    # non-translated edges
 
-    # plot the orf plot
+    for edge in edges:
+        if edge[0] in pos and edge[1] in pos:
+            if graph.edges[edges[edge]].edge_type == "translated":
+                length = pos[edge[1]][0] - pos[edge[0]][0]
+
+                # Translon needs to be positioned at the same height as the
+                # next node downstream and centered using the scaling
+                ds_node_y = pos[graph.nodes[edge[1]].output_nodes[0]][1]
+
+                rect = patches.Rectangle(
+                    (pos[edge[0]][0], ds_node_y - translon_scaling),
+                    length,
+                    translon_height,
+                    linewidth=0.5,
+                    edgecolor='#000000',
+                    facecolor=color_dict["edge_colors"]["frame1"],
+                    )
+                ax1.add_patch(rect)
+
+            else:
+                length = pos[edge[1]][0] - pos[edge[0]][0]
+                rect = patches.Rectangle(
+                    (pos[edge[0]][0], pos[edge[1]][1]),
+                    length,
+                    scantron_height,
+                    linewidth=0.5,
+                    edgecolor='#000000',
+                    facecolor='#000000',
+                    )
+                ax1.add_patch(rect)
+
+    # plot the translon plot
     height = 10
     yticks_heights = []
     yticks_labels = []
     for frame, color in zip(
-        sorted(orfs_in_frame.keys()), [color_dict["edge_colors"]["frame0"], color_dict["edge_colors"]["frame1"], color_dict["edge_colors"]["frame2"]]
+        sorted(translons_in_frame.keys()),
+        [
+            color_dict["edge_colors"]["frame0"],
+            color_dict["edge_colors"]["frame1"],
+            color_dict["edge_colors"]["frame2"]
+            ]
     ):
         yticks_heights.append(height + 1)
         yticks_labels.append("Frame " + str(frame + 1))
         ax2.broken_barh(
-            sorted(orfs_in_frame[frame]),
+            sorted(translons_in_frame[frame]),
             (height, 2),
             facecolors=color,
             edgecolor="black",
@@ -257,14 +301,17 @@ def plot(
     ax2.set_xlim(left=ax1.get_xlim()[0], right=ax1.get_xlim()[1])
     ax2.set_ylim(bottom=10, top=19)
     ax2.set_yticks(yticks_heights, labels=yticks_labels)
-
     plt.show()
     return fig, ax1, ax2
 
 
 if __name__ == "__main__":
     no_node_color_dict = {
-        "edge_colors": {"frame0": "#ffbb8d", "frame1": "#ffeedd", "frame2": "#ffd8be"},
+        "edge_colors": {
+            "frame0": "#ffbb8d",
+            "frame1": "#ffeedd",
+            "frame2": "#ffd8be"
+            },
         "node_colors": {
             "startpoint": "#000000",
             "endpoint": "#000000",
@@ -281,7 +328,9 @@ if __name__ == "__main__":
     # plot(g, color_dict=no_node_color_dict)
 
     g = RDG(name="ATF4 - NM_001675", locus_stop=2041)
-    g.add_open_reading_frame(486, 649)
+    g.add_open_reading_frame(486, 1943)
     g.add_open_reading_frame(700, 891, reinitiation=True)
     g.add_open_reading_frame(888, 1943, reinitiation=True)
+    print(g.newick())
+
     plot(g, color_dict=no_node_color_dict)
