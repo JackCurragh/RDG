63f5eb97807e142d9c473b4aaac88c19da17d146
Jack Tierney <jack.tierney@student.ucc.ie>
2024-01-31 17:05:54 +0000

    fixed newick

--- RDG/plot.py
+++ RDG/plot.py
@@ -206,7 +206,6 @@ def plot(
     for translon in graph.get_translons():
         if translon not in translons:
             translons.append(translon)
-    print(translons)
     name = graph.locus
     locus_stop = graph.locus_stop
     graph = RDG(name=name, locus_stop=locus_stop)
