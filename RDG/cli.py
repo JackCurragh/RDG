80d574a6e630ccb641bd1b5a447d48650492db55
Jack Tierney <jack.tierney@student.ucc.ie>
2024-09-09 09:51:29 +0100

    Refactor code to support multiple input file formats

--- RDG/cli.py
+++ RDG/cli.py
@@ -4,8 +4,9 @@ Console script for RDG.
 Allows interactive command line interface to RDG plot generation 
 """
 import click
+import pandas as pd
 
-from .sequence_to_RDG import build_graphs_from_fasta
+from .sequence_to_RDG import build_graphs_from_fasta, build_graphs_from_bed, build_graphs_from_gtf
 from .plot import plot
 from .RDG_to_file import save, load, newick_to_file
 
@@ -76,6 +77,13 @@ def visualise(
 @rdg_cli.command()
 @click.argument('infile')
 @click.option(
+    '--input_format',
+    '-f',
+    type=click.Choice(['fasta', 'gtf', 'bed'], case_sensitive=False),
+    required=True,
+    help='Input file format (fasta, gtf, bed).'
+)
+@click.option(
     '--start_codons',
     '-s',
     default="ATG,CTG",
@@ -108,19 +116,81 @@ def visualise(
     ''')
 def construct(
         infile,
+        input_format,
         start_codons,
         num_starts,
         min_length,
         reinitiation,
           ):
-    graphs = build_graphs_from_fasta(
+    if input_format == 'fasta':
+        graphs = build_graphs_from_fasta(
+            infile,
+            start_codons=start_codons,
+            num_starts=num_starts,
+            min_length=min_length,
+            reinitiation=reinitiation,
+            )
+        for graph in graphs:
+            save(graph, f"{graph.name}.sqlite")
+
+    elif input_format == 'gtf':
+        graphs = build_graphs_from_gtf(
+            infile,
+            start_codons=start_codons,
+            num_starts=num_starts,
+            min_length=min_length,
+            reinitiation=reinitiation,
+            )
+        for graph in graphs:
+            save(graph, f"{graph.name}.sqlite")
+
+    elif input_format == 'bed':
+        graphs = build_graphs_from_bed(
+            infile,
+            num_starts=num_starts,
+            min_length=min_length,
+            reinitiation=reinitiation,
+            )
+        for graph in graphs:
+            save(graph, f"{graph.name}.sqlite")
+
+
+@rdg_cli.command()
+@click.argument('infile')
+@click.argument('riboseq_bedgraph')
+@click.option(
+    '--min_read_support',
+    '-m',
+    default=10,
+    help='Minimum read support for a node to be kept. Default: 10'
+    )
+def prune(
         infile,
-        start_codons=start_codons,
-        num_starts=num_starts,
-        min_length=min_length,
-        reinitiation=reinitiation,
-        )
+        riboseq_bedgraph,
+        min_read_support,
+        ):
+    '''
+    prune graphs based on riboseq data
+    '''
+    graphs = load(infile)
+    out_graphs = []
+
+    riboseq_df = pd.read_csv(riboseq_bedgraph, sep='\t', header=None)
+    riboseq_df.columns = ['transcript', 'start', 'end', 'count']
     for graph in graphs:
+        riboseq_counts = {} 
+        tx_df = riboseq_df[riboseq_df['transcript'] == graph.name]
+        for node in graph.nodes:
+            riboseq_counts[node] = tx_df[
+                (tx_df['start'] <= node.start) & (tx_df['end'] >= node.end)
+                ]['count'].sum()
+
+        for node in graph.nodes:
+            if riboseq_counts[node] < min_read_support:
+                graph.prune(node)
+        out_graphs.append(graph)
+
+    for graph in out_graphs:
         save(graph, f"{graph.name}.sqlite")
 
 
--- RDG/sequence_to_RDG.py
+++ RDG/sequence_to_RDG.py
@@ -3,6 +3,104 @@ from typing import List, Tuple, Set
 from collections import defaultdict
 from RDG import RDG
 from rich.progress import Progress
+import pandas as pd
+import gff2bed
+from tempfile import TemporaryDirectory
+import tempfile
+import gzip
+import os
+
+
+def is_gzipped(file_path: str) -> bool:
+    """
+    Checks whether the file is gzipped or not
+
+    Inputs:
+        file_path: Path to the file to be checked
+
+    Outputs:
+        True if gzipped, otherwise False
+    """
+    try:
+        with open(file_path, 'rb') as f:
+            # Read the first two bytes of the file
+            header = f.read(2)
+
+        # Check if the file starts with the gzip magic number (0x1f 0x8b)
+        return header == b'\x1f\x8b'
+
+    except IOError:
+        # File not found or unable to open
+        return False
+
+
+def extract_transcript_id(attr_str):
+    for attr in attr_str.split(";"):
+        # Ensembl GFF3 support
+        if attr.startswith("Parent=transcript:") \
+                or attr.startswith("ID=transcript:"):
+            return attr.split(":")[1]
+        # Gencode GFF3 support
+        elif attr.startswith("transcript_id="):
+            return attr.split("=")[1]
+        # Ensembl GTF support
+        elif attr.startswith(" transcript_id "):
+            return attr.split(" ")[2].replace('"', "")
+    return "NA"
+
+
+def convert_to_transcript_coordinates(gtf_df):
+    # Group the dataframe by transcript_id
+    grouped = gtf_df.groupby('transcript_id')
+    converted_dfs = []
+
+    for transcript_id, transcript_df in grouped:
+        # Sort the transcript dataframe by start position
+        transcript_df = transcript_df.sort_values('start')
+        strand = transcript_df['strand'].iloc[0]
+
+        transcript_coord = 0
+        
+        new_df = transcript_df.copy()
+        for idx, row in transcript_df.iterrows():
+            feature_length = row['end'] - row['start'] + 1
+            if strand == '+':
+                new_df.at[idx, 'start'] = transcript_coord
+                new_df.at[idx, 'end'] = transcript_coord + feature_length - 1
+            else:  # strand == '-'
+                new_df.at[idx, 'start'] = transcript_coord
+                new_df.at[idx, 'end'] = transcript_coord + feature_length - 1
+
+            transcript_coord += feature_length
+
+        converted_dfs.append(new_df)
+
+    result_df = pd.concat(converted_dfs)
+
+    return result_df
+
+
+def parse_gff(gff_path: str, num_transcripts: int) -> pd.DataFrame:
+    """
+    Read in the gff file at the provided path and return a dataframe
+
+    Inputs:
+        gff_path: Path to the gff file
+
+    Outputs:
+        gff_df: Dataframe containing the gff information
+    """
+    if is_gzipped(gff_path):
+        with gzip.open(gff_path, 'rt') as f:
+            bed_df = gff2bed.convert(
+                gff2bed.parse(gff_path)
+            )  
+    else:
+        bed_df = gff2bed.convert(
+            gff2bed.parse(gff_path)
+        )
+
+    return bed_df
 
 
 def extract_translons(
@@ -128,5 +226,105 @@ def build_graphs_from_fasta(
                 )
             progress.update(task, advance=1)
             result_graphs.append(dg)
+
+    return result_graphs
+
+
+def build_graphs_from_gtf(file_path: str,
+    min_length: int = 100,
+    reinitiation: bool = False,
+    upstream_limit: int = 1,
+    num_starts: int = 3,
+) -> List[RDG]:
+    """
+    Build graphs from a GTF file.
+
+    Parameters:
+    - file_path (str): The path to the GTF file.
+
+    Returns:
+    List[RDG]: A list of RDG (RDG) objects representing the graphs.
+
+    """
+    df = parse_gff(file_path)
+    transcript_ids = df.iloc[:, 8].apply(
+        lambda x: x.split('transcript_id "')[1].split('"')[0])
+    start_coords = df.iloc[:, 3]
+    stop_coords = df.iloc[:, 4]
+
+    transcripts_df = pd.DataFrame({
+        'Start': start_coords,
+        'Stop': stop_coords
+    })
+
+    result_graphs = []
+    for transcript in transcripts_df['Transcript_ID'].unique():
+        transcript_df = transcripts_df
+        transcript_df = transcript_df.sort_values(by='Start')
+        transcript_df = transcript_df.assign(
+                            Length=transcript_df['Stop'] - transcript_df['Start']
+                            )   
+        transcript_df = transcript_df[transcript_df['Length'] >= min_length]
+
+        graph = RDG(transcript)
+        for index, row in transcript_df.iterrows()[:num_starts]:
+            graph.add_open_reading_frame(
+                row['Start'],
+                row['Stop'],
+                reinitiation=reinitiation,
+                upstream_limit=upstream_limit,
+            )
+        result_graphs.append(graph)
+
+    return result_graphs
+
+
+def build_graphs_from_bed(file_path: str,
+                          min_length: int = 100,
+                          reinitiation: bool = False,
+                          upstream_limit: int = 1,
+                          num_starts: int = 3,
+                        ) -> List[RDG]:
+    """
+    Build graphs from a BED file.
+
+    Parameters:
+    - file_path (str): The path to the BED file.
+
+    Returns:
+    List[RDG]: A list of RDG (RDG) objects representing the graphs.
+
+    """
+    df = pd.read_csv(file_path, sep='\t', header=None)
+    transcript_ids = df.iloc[:, 21]
+    start_coords = df.iloc[:, 10].apply(
+        lambda x: min([int(x) for x in x.split(',')]))
+    stop_coords = df.iloc[:, 11].apply(
+        lambda x: max([int(x) for x in x.split(',')]))
+
+    transcripts_df = pd.DataFrame({
+        'Start': start_coords,
+        'Stop': stop_coords,
+        'Transcript_ID': transcript_ids
+    })
+
+    result_graphs = []
+    for transcript in transcripts_df['Transcript_ID'].unique():
+        transcript_df = transcripts_df[transcripts_df['Transcript_ID'] == transcript]
+        transcript_df = transcript_df.sort_values(by='Start')
+        transcript_df = transcript_df.assign(
+                            Length=transcript_df['Stop'] - transcript_df['Start']
+                            )
+        transcript_df = transcript_df[transcript_df['Length'] >= min_length]
+
+        graph = RDG(transcript)
+        for index, row in transcript_df.iterrows()[:num_starts]:
+            graph.add_open_reading_frame(
+                row['Start'],
+                row['Stop'],
+                reinitiation=reinitiation,
+                upstream_limit=upstream_limit,
+            )
+        result_graphs.append(graph)
 
     return result_graphs
